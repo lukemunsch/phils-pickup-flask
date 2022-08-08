73d4076da785769448cb57c0f0409ade2f6e59dd
Luke Munsch <lpmunsch@gmail.com>
2022-08-08 22:01:12 +0100

    set up ability to view in server window

--- run.py
+++ run.py
@@ -1,20 +1,18 @@
 import os
 import json
-from flask import Flask, render_template, request, flash
-if os.path.exists("env.py"):
-    import env
-
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Hello, World!"
+    print ("Hello from python!")
+    
 
 if __name__ == "__name__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "5000")),
+        port=int(os.environ.get("PORT", "5500")),
         debug=True
     )
