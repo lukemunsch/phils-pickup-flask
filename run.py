747e2921b2a95a19b4b3f7486f40f1ef396fd019
Luke Munsch <lpmunsch@gmail.com>
2022-08-08 23:21:08 +0100

    create new index.html file for main page content

--- run.py
+++ run.py
@@ -1,13 +1,12 @@
 import os
-from flask import Flask
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "hello world"
+    return render_template("index.html")
     
 
 if __name__ == "__main__":
