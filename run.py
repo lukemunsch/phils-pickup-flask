4c80fbb711101ef22b7e21651e09c012ceaa1315
Luke Munsch <lpmunsch@gmail.com>
2022-08-09 22:21:04 +0100

    Set up file structure for additional pages

--- run.py
+++ run.py
@@ -7,7 +7,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-    
+
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
 
 if __name__ == "__main__":
     app.run(
